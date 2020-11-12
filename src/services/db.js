eda4d46b80412a874ba73641ed9dea5187384031
Shahar Harari <shr.harari@gmail.com>
2020-11-12 10:35:49 +0200

    add sample

--- src/services/db.js
+++ src/services/db.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // User: adminMongoDbUser
 // Password: adminPass
 const MONGODB_ROTARY_URI = 'mongodb+srv://adminMongoDbUser:adminPass@rotarynet.zg8h8.mongodb.net/rotary?retryWrites=true&w=majority'
-const MONGODB_SAMPLE_URI = 'mongodb+srv://adminMongoDbUser:adminPass@rotarynet.zg8h8.mongodb.net/sample_mflix?retryWrites=true&w=majority'
+const MONGODB_SAMPLE_URI = 'mongodb://adminMongoDbUser:adminPass@rotarynet.zg8h8.mongodb.net/sample_mflix?retryWrites=true&w=majority'
 // const MONGODB_ROTARY_URI = 'mongodb://heroku_h7zlcmm7:2ajsra7im7oklcfbn4024hkh7i@ds149875.mlab.com:49875/heroku_h7zlcmm7'
 
 const mongoose = require('mongoose');
@@ -14,7 +14,7 @@ const mongoose = require('mongoose');
 
   // var URL = process.env.MONGODB_SAMPLE_URI || 'mongodb://localhost/rotary';
 
-  var URL_TEST = MONGODB_ROTARY_URI || MONGODB_SAMPLE_URI || 'mongodb://localhost/rotary';
+  var URL_TEST = MONGODB_SAMPLE_URI || 'mongodb://localhost/rotary';
 
 mongoose.connect(URL_TEST, {
     useNewUrlParser: true,
