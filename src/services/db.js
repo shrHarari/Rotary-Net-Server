964c554161188fbc28fdeb26c2ccd359f9a11d6e
Shahar Harari <shr.harari@gmail.com>
2020-11-12 10:49:37 +0200

    add sample

--- src/services/db.js
+++ src/services/db.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 // Cluster Name: RotaryNet
 // User: adminMongoDbUser
 // Password: adminPass
-const MONGODB_ROTARY_URI = 'mongodb+srv://adminMongoDbUser:adminPass@rotarynet.zg8h8.mongodb.net/rotary?retryWrites=true&w=majority'
-const MONGODB_SAMPLE_URI = 'mongodb://adminMongoDbUser:adminPass@rotarynet.zg8h8.mongodb.net/sample_mflix?retryWrites=true&w=majority'
+// const MONGODB_ROTARY_URI = 'mongodb+srv://adminMongoDbUser:adminPass@rotarynet.zg8h8.mongodb.net/rotary?retryWrites=true&w=majority'
+// const MONGODB_SAMPLE_URI = 'mongodb+srv://adminMongoDbUser:adminPass@rotarynet.zg8h8.mongodb.net/sample_mflix?retryWrites=true&w=majority'
+const MONGODB_ROTARY_URI = 'mongodb+srv://adminMongoDbUser:adminPass@rotarynet.zg8h8.mongodb.net/rotary?authSource=admin&replicaSet=atlas-1zs1z6-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true'
 // const MONGODB_ROTARY_URI = 'mongodb://heroku_h7zlcmm7:2ajsra7im7oklcfbn4024hkh7i@ds149875.mlab.com:49875/heroku_h7zlcmm7'
 
 const mongoose = require('mongoose');
@@ -16,7 +17,7 @@ const mongoose = require('mongoose');
 
   var URL_TEST = MONGODB_SAMPLE_URI || 'mongodb://localhost/rotary';
 
-mongoose.connect(URL_TEST, {
+mongoose.connect(MONGODB_ROTARY_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     retryWrites: false});
