067be20747e4fd1067de572bc3fe826b6022931f
Shahar Harari <shr.harari@gmail.com>
2020-11-12 11:19:32 +0200

    add connection

--- src/services/db.js
+++ src/services/db.js
@@ -1,23 +1,21 @@
 require('dotenv').config();
 
+////////////////////////////////
 // CONNECTION STRING:
+//------------------------------
 // Cluster Name: RotaryNet
 // User: adminMongoDbUser
 // Password: adminPass
+////////////////////////////////
+
 const MONGODB_ROTARY_URI = 'mongodb+srv://adminMongoDbUser:adminPass@rotarynet.zg8h8.mongodb.net/rotary?retryWrites=true&w=majority'
-const MONGODB_SAMPLE_URI = 'mongodb+srv://adminMongoDbUser:adminPass@rotarynet.zg8h8.mongodb.net/sample_mflix?retryWrites=true&w=majority'
-// const MONGODB_ROTARY_URI = 'mongodb+srv://adminMongoDbUser:adminPass@rotarynet.zg8h8.mongodb.net/rotary?authSource=admin&replicaSet=atlas-1zs1z6-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true'
+// const MONGODB_SAMPLE_URI = 'mongodb+srv://adminMongoDbUser:adminPass@rotarynet.zg8h8.mongodb.net/sample_mflix?retryWrites=true&w=majority'
 // const MONGODB_ROTARY_URI = 'mongodb://heroku_h7zlcmm7:2ajsra7im7oklcfbn4024hkh7i@ds149875.mlab.com:49875/heroku_h7zlcmm7'
 
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/rotary', {
+
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotary', {
-
-  // var URL = process.env.MONGODB_SAMPLE_URI || 'mongodb://localhost/rotary';
-
-  var URL_TEST = MONGODB_ROTARY_URI || 'mongodb://localhost/rotary';
-
-mongoose.connect(URL_TEST, {
+mongoose.connect(MONGODB_ROTARY_URI || 'mongodb://localhost/rotary', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     retryWrites: false});
