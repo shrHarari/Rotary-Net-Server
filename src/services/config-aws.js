a11d99303a17b1e074de0724c6a05caf0aceea4d
Shahar Harari <shr.harari@gmail.com>
2020-11-18 09:12:28 +0200

    AWS

--- src/services/config-aws.js
+++ src/services/config-aws.js
@@ -1,10 +1,6 @@
 
 
-
-// module.exports.accessKeyId = "AKIAJA77SV2WWV7CHYCQ";
 module.exports.accessKeyId = process.env.AWSAccessKeyId
-
-// module.exports.secretAccessKey = "EAmRWmwGd9y5/w5bTXj1Wd92FXtN30CKhpXBa3ve";
 module.exports.secretAccessKey = process.env.AWSSecretKey
 
 module.exports.bucketName = "rotary-net-person-card-images";
