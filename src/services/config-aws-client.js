b1e8d5038a27089a0d6538a36b09ca5940dd562b
Shahar Harari <shr.harari@gmail.com>
2020-11-18 13:33:31 +0200

    AWS

--- src/services/config-aws-client.js
+++
@@ -1,7 +0,0 @@
-
-
-module.exports.accessKeyId = process.env.AWSAccessKeyId
-module.exports.secretAccessKey = process.env.AWSSecretKey
-
-module.exports.roteryNetBucketName = process.env.S3_ROTARY_NET_BUCKET;
-module.exports.region = "us-east-1";
